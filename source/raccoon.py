31b8638a37117efbfe7018e1970ace4d278bd0d8
Pony Satan <ponesatan@gmail.com>
2021-09-27 18:24:03 +0200

    recombobulate scripts directory and yaml

--- source/raccoon.py
+++ source/raccoon.py
@@ -7,5 +7,6 @@ import pynxm
 
 nexus_token=os.getenv('NEXUS_API_TOKEN')
 discord_toekn=os.getenv('DISCORD_API_TOKEN')
+
 #max_backup_count=os.getenv('RACC_BACK_COUNT_MAX')
 #beckup_interval=os.getenv('RACC_BACK_INTERVAL')
